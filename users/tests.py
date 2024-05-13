1823e00fe6bebfa25a5d3e0fec4c65dbc8ba557e
Gustavo Tempel <gtempel@hotmail.com>
2024-05-13 20:23:21 +0200

    Added test for User model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserModelTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_user(self):
+
+        User.objects.create(
+            email = "test@email.com",
+            first_name = "First Name",
+            last_name = "Last Name",
+            phone = "111 22 333 4444",
+            hobbies = list([
+                "Football",
+                "Numismatics",
+                "Videogames",
+                ]
+            ),
+        )
+
+        test_user = User.objects.get(email="test@email.com")
+
+        self.assertEqual(test_user.first_name, "First Name")
+        self.assertEqual(test_user.last_name, "Last Name")
+        self.assertEqual(test_user.phone, "111 22 333 4444")
+        self.assertListEqual(test_user.hobbies, list(["Football", "Numismatics", "Videogames", ]))
+        self.assertIsNone(test_user.email_validated_at)
+        self.assertIsNone(test_user.phone_validated_at)
